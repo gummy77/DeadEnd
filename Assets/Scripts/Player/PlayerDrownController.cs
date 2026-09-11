ce1b564f4bd00867b583d8f2c80578b7dec180fb
liz <elizabeth.lawler2003@gmail.com>
2026-09-11 15:14:45 +1200

    drown

---
+++ Assets/Scripts/Player/PlayerDrownController.cs
@@ -0,0 +1,43 @@
+using System;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace Player
+{
+    public class PlayerDrownController : MonoBehaviour
+    {
+        [SerializeField] private Transform playerTransform;
+        [SerializeField] private Volume drownVolume;
+        
+        [SerializeField] private float waterLevel;
+        [SerializeField] private float drownTime;
+        
+        private float _drownTimer;
+
+        private void Update()
+        {
+            if (playerTransform.position.y < waterLevel)
+            {
+                _drownTimer += Time.deltaTime;
+            }
+            else
+            {
+                if (_drownTimer > 0)
+                {
+                    _drownTimer -= Time.deltaTime;
+                }
+            }
+            
+            drownVolume.weight = _drownTimer / drownTime;
+            
+            if (_drownTimer > drownTime)
+            {
+                GameObject respawn = GameObject.Find("Respawn");
+                if (respawn)
+                {
+                    playerTransform.position = respawn.transform.position;
+                }
+            }
+        }
+    }
+}
